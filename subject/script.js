0e7797d7a94afadb56d80cc1a04f9ef5ff62391d
Nejma Araki <nejma.araki@gmail.com>
2024-03-22 23:46:29 +0100

    commit de Nejma ARAKI

--- subject/script.js
+++ subject/script.js
@@ -1,5 +1,60 @@
 // NOTE: 
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
-// # START EDITING YOUR JAVASCRIPT HERE
+// Projet de Nejma ARAKI
+
+function appendToDisplay(value) {
+    var display = document.querySelector('.calculator__display');
+    display.textContent += value;
+}
+
+function clear() {
+    var display = document.querySelector('.calculator__display');
+    display.textContent = '0';
+}
+
+function calcul() {
+    var display = document.querySelector('.calculator__display');
+    display.textContent = eval(display.textContent); 
+}
+
+function un_decimal() {
+    var display = document.querySelector('.calculator__display');
+    if (!display.textContent.includes('.')) {
+        display.textContent += '.';
+    }
+}
+
+document.querySelectorAll('.calculator__keys button').forEach(button => {
+    button.addEventListener('click', function() {
+        var action = this.getAttribute('data-action');
+        var value = this.textContent;
+        
+        if (action === 'clear') {
+            clear(); 
+        } else if (action === 'calculate') {
+            calcul(); 
+        } else if (action === 'decimal') {
+            un_decimal(); 
+        }
+    });
+});
+
+// malheureusement, la suite qui consiste à effectuer les opérations ne marche pas 
+function operation(num1, num2, op) {
+    switch(op) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case 'x':
+            return num1 * num2;
+        case '÷':
+            return num1 / num2;
+        default:
+            return 'Error';
+    }
+}
+
+
 // ===============
